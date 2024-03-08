3a25cdd750fc3f68c450425f1434262ccaae938a
mohammedazez <muhamadaziz047@gmail.com>
2024-03-08 10:17:00 +0700

    move code and find max

--- main.go
+++ main.go
@@ -1,11 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+// Nomor 1
+func IsPalindrome(str string) bool {
+	lowerCase := strings.ToLower(str)
+	for i := 0; i < len(string(lowerCase))/2; i++ {
+		if string(lowerCase[i]) != string(lowerCase[len(lowerCase)-1-i]) {
+			return false
+		}
+	}
+	return true
+}
+
+// Nomor 2
+func FindMax(arr []int) int {
+	max := arr[0]
+	for _, num := range arr {
+		if num > max {
+			max = num
+		}
+	}
+	return max
+}
 
 func main() {
-	// ispalindrome
+	// IsPalindrome
 	fmt.Println(IsPalindrome("abba"))
 	fmt.Println(IsPalindrome("kodok"))
 	fmt.Println(IsPalindrome("radar"))
 	fmt.Println(IsPalindrome("hello"))
+
+	// FindMax
+	input := []int{3, 5, 1, 9, 2}
+	fmt.Println(FindMax(input))
+
 }
