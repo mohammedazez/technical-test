f7db82849ee16ca1de238cf77b83a18c09f1a738
mohammedazez <muhamadaziz047@gmail.com>
2024-03-08 10:03:39 +0700

    go mod init

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	// ispalindrome
+	fmt.Println(IsPalindrome("abba"))
+	fmt.Println(IsPalindrome("kodok"))
+	fmt.Println(IsPalindrome("radar"))
+	fmt.Println(IsPalindrome("hello"))
+}
